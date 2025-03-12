95ccac42108a318ab5f6c1ae6169adbff3a30c3f
Your Name <your-email@example.com>
2025-03-12 14:13:51 +0200

    Initial Commit

---
+++ deemoTests/deemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  deemoTests.swift
+//  deemoTests
+//
+//  Created by Zhibek Nasipbekova on 12.03.2025.
+//
+
+import Testing
+
+struct deemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
